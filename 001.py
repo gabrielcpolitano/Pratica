aac8576613dcbddbf42b26d80bb3c76be07c6795
Gabriel Correia Politano <75918121+gabrielcpolitano@users.noreply.github.com>
2024-01-04 16:17:33 -0300

    Delete 001.py

--- 001.py
+++
@@ -1,6 +0,0 @@
-#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
-
-from math import ceil
-a = float(input('Digite um número quebrado: '))
-b = ceil(a)
-print(f'Esse é o número quebrado: {a}. Esse é o número inteiro {b}')
